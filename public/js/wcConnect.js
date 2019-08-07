d930c40f2a8dd7b57c843e0ba9d5b0f531a01721
laiharry <lai.harry@gmail.com>
2019-08-07 22:45:14 +0800

    Add files via upload

--- public/js/wcConnect.js
+++ public/js/wcConnect.js
@@ -29,7 +29,7 @@ class WCConnect {
 //            console.log(`ws://${this.IP}:${this.PORT}`);
 
 //            var s = new WebSocket(((window.location.protocol === "https:") ? "wss://" : "ws://") + window.location.host + "/ws");
-//			socket = new WebSocket("ws://localhost:8080/ws");
+//			this.socket = new WebSocket("ws://localhost:8080/ws");
             this.socket = new WebSocket("ws://rps-rps.apps.us-east-2.online-starter.openshift.com/ws");  
 //            this.socket = new WebSocket(`ws://${this.IP}:${this.PORT}/ws`);
 //            this.socket = new WebSocket(`ws://localhost:3000/ws`);
